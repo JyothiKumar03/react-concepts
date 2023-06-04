5e12bf7d4ea7bf1b7e985348b99f84b77a3e1f1a
Jyothi Kumar <88045362+JyothiKumar03@users.noreply.github.com>
2023-06-04 18:37:42 +0530

    Update usestate-hook.js

--- usestate-hook.js
+++ usestate-hook.js
@@ -1,3 +1,15 @@
+/*
+useState Hook:
+
+-The useState hook is a built-in hook in React that allows functional components to have state.
+-It returns an array with two elements: the current state value and a function to update that state.
+-The initial state value is passed as an argument to the useState hook during component initialization.
+-The state value can be of any data type, such as a number, string, boolean, object, or an array.
+-Whenever the state is updated using the update function, the component will re-render with the new state value.
+-State updates are asynchronous and are batched together for better performance.
+-Multiple state variables can be used in a single component by calling the useState hook multiple times.
+*/
+
 import React, { useState } from "react";
 //styling is optional, you can ignore this part of code
 const countStyle = {
